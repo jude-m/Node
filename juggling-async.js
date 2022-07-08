ec4cb23ff6b247102f15f61feb4a14f95f6a095c
jude-m <jude.mahipalamudali@outlook.com>
2022-07-09 01:58:20 +0530

    added juggling-async.js

--- juggling-async.js
+++ juggling-async.js
@@ -2,15 +2,25 @@ const http = require('http');
 const bl = require('bl')
 
 var list = [];
+var count = 0;
 
-for (let i = 2; i< process.argv.length; i++) {
-    http.get(process.argv[i], (res) => {
+for (let i = 2; i < process.argv.length; i++) {
+    httpGet(i);
+}
+
+function httpGet(index) {
+    http.get(process.argv[index], (res) => {
         res.pipe(bl((err, data) => {
-            data = data.toString();
-            
-            list.push("kkkk");                       
+            if (err) {
+                return console.error(err)
+            }
+
+            list[index] = data.toString();
+            count++;
+
+            if (count === process.argv.length - 2) {
+                list.forEach((list) => console.log(list));
+            }
         }))
     });
 }
-
-console.log(list[0]);
